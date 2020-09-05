72f8f6dac9c63021b3dc37f1cf845c8c7ea73819
Gutemberg Praia <gutembergpraia@gmail.com>
2020-09-05 16:28:04 -0400

    Alteração da função de utilização do servo, Saparado a função de leitura dos sensores de portões e garagens e correção de bugs.
    - Alterado a função de utilização do servo de write() para writeMicroseconds(), para teruma melhor precisão.
    - Alteração na gate_type para suporta micro segundos ao inves de angulo.
    -Separado o processo de leitura dos sensores dos portões e das garagens e correções de bugs
    - Adicionado as funções:
     printDisplay(int iStatus) - Função que é responsavel por escrever no Display.
    void updateConts() - Função que  é responsavel por atualizar os valores dos contadores no loop.

--- code/estacionamento/estacionamento.ino
+++ code/estacionamento/estacionamento.ino
@@ -12,6 +12,9 @@ LiquidCrystal lcd(13, 12, 11, 8, 1, 0);
 
 unsigned long currentMillis;
 
+int iPortao = 0;
+int iGaragem = 0;
+
 //Estrutura garagem
 typedef struct
 {
@@ -43,9 +46,9 @@ typedef struct
    int iDistMin;  //distancia minima de acionamento
    int iValue;    //valor da ultima leitura
    int iServo;    //porta do servo
-   int iAngIni;   //angulo inicial
-   int iAngFin;   //angulo Final
-   int iStatus;   //0-StandBy  1-Open 2-Close
+   int iMsIni;   //micro segundos de aciomaneto inicial
+   int iMsFin;   //micro segundos de aciomaneto Final
+   int iStatus;   //0-Close  1-Open
    unsigned long previousMillis; //tempo da ultima execução
    const long interval; //Tempo de espera ate o fechamento em millisegundos
 
@@ -53,53 +56,83 @@ typedef struct
 
 //Iniciar vetor portão de entrada/saida
 gate_type gate[2]={
-  {0,0,A0,300,0,9,90,0,0,0,5000},
-  {1,1,A1,500,0,10,90,0,0,0,5000}
+  {0,0,A0,300,0,1500,300,0,0,0,5000},
+  {1,1,A1,500,0,1500,300,0,0,0,5000}
 };
 
 
-void readSensor (){
-  for (int i = 0; i < TOTAL_PORTAO; i++) {
-    gate[i].iValue = analogRead(gate[i].iSensor);
-    if(gate[i].iValue < gate[i].iDistMin && (iGaragemDisp > 0 || gate[i].iTipo==1)){
-      if(gate[i].iStatus == 0){
-        sPortao[gate[i].iServoId].write(gate[i].iAngFin);
-        gate[i].iStatus = 1;
-        gate[i].previousMillis = currentMillis;
-        delay(10);
-      }
-    }else{
-      if(gate[i].iStatus == 1){
-        if (currentMillis - gate[i].previousMillis >= gate[i].interval) {
-          sPortao[gate[i].iServoId].write(90);
-          gate[i].iStatus = 0;
-          if(gate[i].iTipo){
+//Função resposavel pela leitura dos sensores
+void readSensorPortao (){
+  gate[iPortao].iValue = analogRead(gate[iPortao].iSensor);
+  
+  if(gate[iPortao].iValue < gate[iPortao].iDistMin && ((gate[iPortao].iTipo==0 && iGaragemDisp > 0)||(gate[iPortao].iTipo==1 && (iGaragemDisp >= 0 && iGaragemDisp < TOTAL_GARAGEM)))){
+  	sPortao[gate[iPortao].iServoId].writeMicroseconds(gate[iPortao].iMsFin);
+    gate[iPortao].previousMillis = currentMillis;
+  	gate[iPortao].iStatus = 1;
+  }else{
+    if (currentMillis - gate[iPortao].previousMillis >= gate[iPortao].interval) {
+  	  sPortao[gate[iPortao].iServoId].writeMicroseconds(gate[iPortao].iMsIni);
+      if(gate[iPortao].iStatus == 1){
+    	  gate[iPortao].iStatus = 0;
+          if(gate[iPortao].iTipo == 1){
             iGaragemDisp+=1;
           }else{
             iGaragemDisp-=1;
           }
-          lcd.setCursor(13, 1);
-      lcd.print(iGaragemDisp);
-
-          delay(10);
-
-        }
+      	printDisplay(2);
       }
-    }
-  }
-
-  for (int i = 0; i < TOTAL_GARAGEM; i++) {
-    garagem[i].iValue = analogRead(garagem[i].iSensor);
-    if(garagem[i].iValue < garagem[i].iDistMin){
-      digitalWrite(garagem[i].iLedGreen, HIGH);
-      digitalWrite(garagem[i].iLedRed, LOW);
-    }else{
-      digitalWrite(garagem[i].iLedGreen, LOW);
-      digitalWrite(garagem[i].iLedRed, HIGH);
     }
   }
 }
 
+void readSensorGaragem (){
+  garagem[iGaragem].iValue = analogRead(garagem[iGaragem].iSensor);
+    if(garagem[iGaragem].iValue < garagem[iGaragem].iDistMin){
+      digitalWrite(garagem[iGaragem].iLedGreen, HIGH);
+      digitalWrite(garagem[iGaragem].iLedRed, LOW);
+    }else{
+      digitalWrite(garagem[iGaragem].iLedGreen, LOW);
+      digitalWrite(garagem[iGaragem].iLedRed, HIGH);
+    }
+}
+
+/*
+* Função resposavel pela escrita no dispaly
+*  Paramentros de entrada: 1 - Primeira escrita - escreve todos os dados
+*                          2 - Atualização - atualiza a quantidade de vagas 
+*/
+void printDisplay(int iStatus){
+  switch (iStatus) {
+    case 1:
+        lcd.begin(16, 2);
+        lcd.print("Seja Bem vindo!");
+        lcd.setCursor(0, 1);
+        lcd.print("Vagas Disp: 0");
+        lcd.setCursor(13, 1);
+        lcd.print(iGaragemDisp);
+      break;
+    case 2:
+        lcd.setCursor(13, 1);
+        lcd.print(iGaragemDisp);
+      break;
+  }
+}
+
+void updateConts(){
+ 
+  if(iPortao == TOTAL_PORTAO-1){
+  	iPortao = 0;
+  }else{
+    iPortao += 1;
+  }
+
+  if(iGaragem == TOTAL_GARAGEM-1){
+  	iGaragem = -1;
+  }else{
+    iGaragem += 1;
+  }
+
+}
 
 
 
@@ -110,35 +143,33 @@ void setup() {
   for (int i = 0; i < TOTAL_GARAGEM; i++) {
     pinMode(garagem[i].iLedGreen, OUTPUT);
     pinMode(garagem[i].iLedRed, OUTPUT);
-  }
-
-  //iniciar servos
-  for (int i = 0; i < TOTAL_PORTAO; i++) {
-    sPortao[gate[i].iServoId].attach(gate[i].iServo);
-    sPortao[gate[i].iServoId].write(gate[i].iAngIni);
-    delay(10);
-  }
-   
-  //Verifica se possui algum carro na garagem ao iniciar o sistema
-  for (int i = 0; i < TOTAL_GARAGEM; i++) {
+    
+     //Verifica se possui algum carro na garagem ao iniciar o sistema
     garagem[i].iValue = analogRead(garagem[i].iSensor);
     if(garagem[i].iValue < garagem[i].iDistMin){
       iGaragemDisp-=1;
     }
   }
 
-  lcd.begin(16, 2);
-  lcd.print("Seja Bem vindo!");
-  lcd.setCursor(0, 1);
-  lcd.print("Vagas Disp: 0");
-  lcd.setCursor(13, 1);
-  lcd.print(iGaragemDisp);
+  //iniciar servos
+  for (int i = 0; i < TOTAL_PORTAO; i++) {
+    sPortao[gate[i].iServoId].attach(gate[i].iServo);
+    sPortao[gate[i].iServoId].write(gate[i].iMsIni);
+    delay(10);
+  }
+ 
+  printDisplay(1);
 }
 
 void loop() {
 
   currentMillis = millis();
-
-  readSensor();
-  delay(20);
+  
+  readSensorPortao();
+  readSensorGaragem ();
+  
+  updateConts();
+  
+  delay(10);
+  
 }
